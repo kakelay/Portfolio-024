945bdcc4525245ec2d6c7ec7a58a8dbc962ade61
kakelay <kakelay18052002@gmail.com>
2024-01-05 10:21:55 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/kakelay/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.kakelay.portfolio
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_development.dart
@@ -0,0 +1,6 @@
+import 'package:portfolio/app/modules/home_page/home_page.dart';
+import 'package:portfolio/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const HomePageView());
+}
